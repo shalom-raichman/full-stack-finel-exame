b3f5b4315b409945b99f54e6848e514ddf26ea05
Shalom <shlomo000@gmail.com>
2024-12-19 12:26:19 +0200

    reformat func

--- server/src/services/analysis.service.ts
+++ server/src/services/analysis.service.ts
@@ -3,15 +3,21 @@ import AttackTypeModel, { IAttackType } from '../models/attackType.schema'
 
 export const deadliestAttackTypesService = async (attackType?: string) => {
   try {
-    const attacksTypes = await AttackTypeModel.find({}).populate('attacks') as IAttackType[]
-    const reduceAttackTypes = attacksTypes.map((a: IAttackType) => {
-      return {
-        attackType: a.attackType,
-        //@ts-ignore
-        casualties: a.attacks.reduce((sum, a ) => sum + a.nkill + a.nwound, 0)
-      }
-    }).sort((a, b) => b.casualties - a.casualties)
-    console.log(reduceAttackTypes)
+    const attacksTypes = (await AttackTypeModel.find({}).populate(
+      'attacks'
+    )) as IAttackType[]
+    const reduceAttackTypes = attacksTypes
+      .map((a: IAttackType) => {
+        return {
+          attackType: a.attackType,
+          //@ts-ignore
+          casualties: a.attacks.reduce((sum, a) => sum + a.nkill + a.nwound, 0),
+        }
+      })
+      .sort((a, b) => b.casualties - a.casualties)
+
+    if (attackType) return reduceAttackTypes.find((a) => a.attackType == attackType)
+    return reduceAttackTypes
   } catch (err) {
     throw err
   }
