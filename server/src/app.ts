d2fcff1b8bccbd4ff72be12721a3697ced2b521c
Shalom <shlomo000@gmail.com>
2024-12-19 15:30:53 +0200

    sum reformating

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,6 @@
 import analysisRouter from './routes/analysis.route'
 import relationshipsRouter from './routes/relationships.route'
-import  express from 'express'
+import express from 'express'
 import { connentToMongo } from './config/db'
 import cors from 'cors'
 import 'dotenv/config'
@@ -16,7 +16,6 @@ app.use(express.json())
 
 app.use('/api/analysis', analysisRouter)
 app.use('/api/relationships', relationshipsRouter)
-
 
 app.listen(PORT, () => {
   console.log(`[server] is up and runing on port ${PORT}`)
