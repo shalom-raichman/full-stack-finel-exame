b531cc9b3685605abb7a601d7a3a7667beae5593
Shalom <shlomo000@gmail.com>
2024-12-19 17:59:13 +0200

    add get groups by years service

--- server/src/services/relationships.service.ts
+++ server/src/services/relationships.service.ts
@@ -1,6 +1,7 @@
 import { IAttack } from '../models/attack.schema'
 import AttackOrganizationModel from '../models/attackOrganization.schema'
 import AttackRegionModel, { IAttackRegion } from '../models/attackRegion.schema'
+import AttackYearModel, { IAttackYear } from '../models/attackYear.schema'
 
 const getGroupsByRgion = async (region: string) => {
   try {
@@ -9,8 +10,10 @@ const getGroupsByRgion = async (region: string) => {
     }).populate('attacks')) as IAttackRegion
 
     const organizations: { organization: string; attacksNumber: number }[] = []
+
     ;(attacksRegions.attacks as IAttack[]).map((attack: IAttack) => {
       const currentAttack = organizations.find((a) => a.organization == attack.gname)
+
       if (currentAttack) {
         currentAttack.attacksNumber += 1
       } else {
@@ -23,12 +26,14 @@ const getGroupsByRgion = async (region: string) => {
   }
 }
 
-export const TopGroupsService = async (region: string, top: boolean) => {
+export const topGroupsService = async (region: string, top: boolean) => {
   try {
     if (top) {
-      console.log((await getGroupsByRgion(region))
-      .sort((a, b) => b.attacksNumber - a.attacksNumber)
-      .slice(0, 5))
+      console.log(
+        (await getGroupsByRgion(region))
+          .sort((a, b) => b.attacksNumber - a.attacksNumber)
+          .slice(0, 5)
+      )
       return (await getGroupsByRgion(region))
         .sort((a, b) => b.attacksNumber - a.attacksNumber)
         .slice(0, 5)
@@ -36,6 +41,28 @@ export const TopGroupsService = async (region: string, top: boolean) => {
       console.log(await getGroupsByRgion(region))
       return await getGroupsByRgion(region)
     }
+  } catch (err) {
+    throw err
+  }
+}
+
+export const getGroupsByYear = async (year: number) => {
+  try {
+    const attacksRegions = (await AttackYearModel.findOne({
+      attackYear: year,
+    }).populate('attacks')) as IAttackYear
+
+    const organizations: { organization: string; attacksNumber: number }[] = []
+    ;(attacksRegions.attacks as IAttack[]).map((attack: IAttack) => {
+      const currentAttack = organizations.find((a) => a.organization == attack.gname)
+      if (currentAttack) {
+        currentAttack.attacksNumber += 1
+      } else {
+        organizations.push({ organization: attack.gname, attacksNumber: 1 })
+      }
+    })
+    console.log(organizations.sort((a, b) => b.attacksNumber - a.attacksNumber))
+    return organizations.sort((a, b) => b.attacksNumber - a.attacksNumber)
   } catch (err) {
     throw err
   }
