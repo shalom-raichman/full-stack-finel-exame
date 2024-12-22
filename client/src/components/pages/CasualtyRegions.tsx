6860c6d6d886919be4c75591996b4d2f541e6263
Shalom <shlomo000@gmail.com>
2024-12-22 18:49:28 +0200

    add CasualtyRegions compo with map and cordinates

---
+++ client/src/components/pages/CasualtyRegions.tsx
@@ -0,0 +1,55 @@
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import { useEffect, useState } from 'react'
+import { AttackRegionModel } from '../../types/state.model'
+import { getDeadliestRegions } from '../../services/analysis.service'
+import { CircularProgress } from '@mui/material'
+
+const CasualtyRegions = () => {
+  const initialState: AttackRegionModel = {
+    err: false,
+    loading: false,
+    data: [],
+  }
+  const [attackRegions, setAttackRegions] = useState(initialState)
+  useEffect(() => {
+    ;(async () => {
+      setAttackRegions({ ...attackRegions, loading: true })
+      const data = await getDeadliestRegions()
+      setAttackRegions({ loading: false, err: data.err, data: data.data })
+    })()
+  }, [])
+
+  return (
+    <div className='page'>
+      <MapContainer
+        className='map'
+        center={[51.505, -0.09]}
+        zoom={3}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        <input
+          type='text'
+          style={{ zIndex: 900, position: 'absolute', right: 10, top: 10 }}
+        />
+        {attackRegions.loading ? (
+          <CircularProgress className='loading' />
+        ) : (
+          attackRegions.data.map((a) => (
+            <Marker key={a._id} position={[a.latitude, a.longitude]}>
+              <Popup key={a._id}>
+                {'City: ' + a.attackRegion} <br /> {'Casualties:' + a.casualties}
+              </Popup>
+            </Marker>
+          ))
+        )}
+      </MapContainer>
+    </div>
+  )
+}
+
+export default CasualtyRegions
