42dac8e192a7300c9aea0ad79711e3e3fded8035
Shalom <shlomo000@gmail.com>
2024-12-22 18:48:39 +0200

    add a multy select filter, don't work with '/' in the url

--- client/src/components/pages/AttackTypes.tsx
+++ client/src/components/pages/AttackTypes.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { getDeadliestAttackTypes } from '../../services/analysis.service'
 import { BarChart } from '@mui/x-charts'
 import { AttackTypeModel } from '../../types/state.model'
+import { Autocomplete, CircularProgress, TextField } from '@mui/material'
 
 const AttackTypes = () => {
   const initialSatate: AttackTypeModel = {
@@ -10,17 +11,42 @@ const AttackTypes = () => {
     loading: false,
   }
   const [attackTypes, setAttackTypes] = useState<AttackTypeModel>(initialSatate)
+  const [attacksData, setAttacksData] = useState<typeof initialSatate.data>([])
   useEffect(() => {
     ;(async () => {
       setAttackTypes({ ...attackTypes, loading: true })
       const data = await getDeadliestAttackTypes()
       setAttackTypes({ ...attackTypes, loading: false, data: data.data })
+      setAttacksData(data.data)
     })()
   }, [])
 
+  const handelSelect = async (e: any, newValue: any) => {
+    const params =
+      newValue.length > 0 ? newValue.map((a: any) => a.attackType) : ['any-type']
+    console.log(newValue.map((a: any) => a.attackType))
+    setAttackTypes({ ...attackTypes, loading: true })
+    const data = await getDeadliestAttackTypes(params)
+    setAttackTypes({ ...attackTypes, loading: false, data: data.data })
+  }
+
   return (
-    <div className='page'>
-      <div>{attackTypes.loading ? <div>loading</div> : JSON.stringify(attackTypes)}</div>
+    <div className='page charts-page'>
+      {attackTypes.loading && <CircularProgress className='loading' />}
+      <Autocomplete
+        size='small'
+        multiple
+        limitTags={2}
+        id='multiple-limit-tags'
+        options={attacksData}
+        getOptionLabel={(option) => option.attackType}
+        renderInput={(params) => (
+          <TextField {...params} label='Attack Types' placeholder='most populer' />
+        )}
+        sx={{ width: '300px' }}
+        onChange={(e, v) => handelSelect(e, v)}
+      />
+
       <BarChart
         width={1000}
         height={300}
