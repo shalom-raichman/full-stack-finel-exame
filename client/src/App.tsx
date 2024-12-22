2f9f315c2c833b773d592c286739052944f96cd2
Shalom <shlomo000@gmail.com>
2024-12-22 16:21:21 +0200

    add rout to CasualtyRegions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Pages from './components/pages/Pages'
 import AttackTypes from './components/pages/AttackTypes'
+import CasualtyRegions from './components/pages/CasualtyRegions'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Pages />}>
-          <Route path="attackTypes" element={<AttackTypes />} />
-        {/* <Route index element={<TanksList />} />
-        <Route path="orders" element={<OrdersList />} />
+          <Route path='attackTypes' element={<AttackTypes />} />
+          <Route path='CasualtyRegions' element={<CasualtyRegions />} />
+          {/* <Route path="orders" element={<OrdersList />} />
         <Route path="inventory" element={<InventoryList />} />
         <Route path="charts" element={<Deshboard />} /> */}
         </Route>
