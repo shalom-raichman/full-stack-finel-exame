7ccc00f5448c16232fc4468c9835befa9ad8d4d8
Shalom <shlomo000@gmail.com>
2024-12-22 14:45:39 +0200

    add a defaulte value when sending the req

--- client/src/services/analysis.service.ts
+++ client/src/services/analysis.service.ts
@@ -1,8 +1,10 @@
 const BASE_URL = 'http://localhost:1414/'
 
-export const getDeadliestAttackTypes = async (attackType?: string) => {
+export const getDeadliestAttackTypes = async (attackType = ['any-type']) => {
   try {
-    const res = await fetch(`${BASE_URL}api/analysis/deadliest-attack-types/`)
+    const res = await fetch(
+      `${BASE_URL}api/analysis/deadliest-attack-types/${JSON.stringify(attackType)}`
+    )
     const data = await res.json()
     return data
   } catch (err) {
