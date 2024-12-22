9197440d852ab3ebd32166188d2493974b2bbbc3
Shalom <shlomo000@gmail.com>
2024-12-22 15:09:11 +0200

    remove unneccery ep

--- server/src/routes/analysis.route.ts
+++ server/src/routes/analysis.route.ts
@@ -7,8 +7,6 @@ router.get('/deadliest-attack-types/:attackType', deadliestAttackTypes)
 
 router.get('/highest-casualty-regions/:region', highestCasualtyRegions)
 
-router.get('/highest-casualty-regions', highestCasualtyRegions)
-
 router.get('/incident-trends/', () => {})
 
 
