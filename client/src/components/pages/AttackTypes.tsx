98562fd34cec1b3bbbc950b23bd88bc449d1db9c
Shalom <shlomo000@gmail.com>
2024-12-22 13:44:08 +0200

    add base AttackTypes component

--- client/src/components/pages/AttackTypes.tsx
+++ client/src/components/pages/AttackTypes.tsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from 'react'
+import { getDeadliestAttackTypes } from '../../services/analysis.service'
+import { BarChart } from '@mui/x-charts'
+import { AttackTypeModel } from '../../types/state.model'
 
 const AttackTypes = () => {
+  const initialSatate: AttackTypeModel = {
+    err: false,
+    data: [],
+    loading: false,
+  }
+  const [attackTypes, setAttackTypes] = useState<typeof initialSatate>(initialSatate)
+  useEffect(() => {
+    ;(async () => {
+      setAttackTypes({ ...attackTypes, loading: true })
+      const data = await getDeadliestAttackTypes()
+      setAttackTypes({ ...attackTypes, loading: false, data: data.data })
+    })()
+  }, [])
+
   return (
-    <div className='page'>AttackTypes</div>
+    <div className='page'>
+      <div>{attackTypes.loading ? <div>loading</div> : JSON.stringify(attackTypes)}</div>
+      <BarChart
+        width={500}
+        height={300}
+        series={[
+          {
+            data: attackTypes.data.map((a) => a.casualties),
+            id: 'uvId',
+            stack: 'total',
+          },
+        ]}
+        xAxis={[{ data: attackTypes.data.map((a) => a.attackType), scaleType: 'band' }]}
+      />
+    </div>
   )
 }
 
