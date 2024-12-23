2a70005be1fa2636faaf0039523b659e2fb405e2
Shalom <shlomo000@gmail.com>
2024-12-23 14:06:15 +0200

    add getDeadliestYears fetch func in the client

--- client/src/services/analysis.service.ts
+++ client/src/services/analysis.service.ts
@@ -1,3 +1,5 @@
+import { AttackYearDto } from '../types/attackYear.DTO'
+
 const BASE_URL = 'http://localhost:1414/'
 
 export const getDeadliestAttackTypes = async (attackType = ['any-type']) => {
@@ -16,6 +18,18 @@ export const getDeadliestRegions = async (attackType = 'any-type') => {
   try {
     const res = await fetch(
       `${BASE_URL}api/analysis/highest-casualty-regions/${attackType}`
+    )
+    const data = await res.json()
+    return data
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const getDeadliestYears = async (attackYear: AttackYearDto) => {
+  try {
+    const res = await fetch(
+      `${BASE_URL}api/analysis/incident-trends/?filter=${JSON.stringify(attackYear)}`
     )
     const data = await res.json()
     return data
