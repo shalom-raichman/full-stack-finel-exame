7185c0b29a859a8af0d26e3baa893bb6d4eb2212
Shalom <shlomo000@gmail.com>
2024-12-23 15:25:58 +0200

    wire top groups route and controller

---
+++ server/src/controllers/relationships.controllers.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express'
+import { topGroupsService } from '../services/relationships.service'
+
+export const topGroups = async (req: Request, res: Response) => {
+  try {
+    console.log(req.params.region)
+    const data = await topGroupsService(req.params.region, JSON.parse(req.params.top))
+    res.json({
+      err: false,
+      message: 'Here is the deadliest organizations in the region',
+      data: data,
+    })
+  } catch (err) {
+    res.json({
+      err: true,
+      message: (err as Error).message,
+      data: null,
+    })
+  }
+}
--- server/src/routes/relationships.route.ts
+++ server/src/routes/relationships.route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
+import { topGroups } from '../controllers/relationships.controllers'
 
 const router = Router()
 
-router.get('/top-groups/', () => {})
+router.get('/top-groups/:region/:top', topGroups)
 
 router.get('/groups-by-year/', () => {})
 
