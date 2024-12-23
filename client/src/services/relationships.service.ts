4427801b82b02a14924e78138c42184f9454ed4d
Shalom <shlomo000@gmail.com>
2024-12-23 17:19:06 +0200

    add getRegions utils func

--- client/src/services/relationships.service.ts
+++ client/src/services/relationships.service.ts
@@ -8,7 +8,18 @@ export const getTopGroupsByRegion = async (attackOrganization: AttackOrganizatio
       `${BASE_URL}api/relationships/top-groups/${attackOrganization.region}/${JSON.stringify(attackOrganization.top)}`
     )
     const data = await res.json()
-    console.log(data)
+    return data
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const getRegions = async () => {
+  try {
+    const res = await fetch(
+      `${BASE_URL}api/relationships/regions`
+    )
+    const data = await res.json()
     return data
   } catch (err) {
     console.error(err)
