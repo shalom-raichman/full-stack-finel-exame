08194525f6e9c5a2a398d1f7f8fbad13371ecc8a
Shalom <shlomo000@gmail.com>
2024-12-23 14:04:14 +0200

    add incidentTrends controller func

--- server/src/controllers/analysis.controller.ts
+++ server/src/controllers/analysis.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { deadliestAttackTypesService, highestCasualtyRegionsService } from '../services/analysis.service'
+import { deadliestAttackTypesService, highestCasualtyRegionsService, incidentTrendsService } from '../services/analysis.service'
 
 export const  deadliestAttackTypes = async (req: Request, res: Response) => {
   const data = await deadliestAttackTypesService(JSON.parse(req.params.attackType))
@@ -21,6 +21,25 @@ export const  deadliestAttackTypes = async (req: Request, res: Response) => {
 export const highestCasualtyRegions = async (req: Request, res: Response) => {
   try {
     const data = await highestCasualtyRegionsService(req.params.region)
+    res.json({
+      err: false,
+      message: 'Here is the deadliest regions',
+      data: data,
+    })
+  } catch (err) {
+    res.json({
+      err: true,
+      message: (err as Error).message,
+      data: null,
+    })
+  }
+}
+
+export const incidentTrends = async (req: Request, res: Response) => {
+  try {
+    const query = JSON.parse((req.query.filter as string))
+    console.log(query)
+    const data = await incidentTrendsService(query)
     res.json({
       err: false,
       message: 'Here is the deadliest regions',
