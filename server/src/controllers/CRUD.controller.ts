08546b001ff68a1a6194cf1d910b7adea81a3151
Shalom <shlomo000@gmail.com>
2024-12-24 15:44:43 +0200

    add update attack ep

--- server/src/controllers/CRUD.controller.ts
+++ server/src/controllers/CRUD.controller.ts
@@ -3,6 +3,23 @@ import { createUpdateAttackService } from '../services/CRUD.service'
 
 export const createAttack = async (req: Request, res: Response) => {
   try {
+    const data = await createUpdateAttackService(req.body, true)
+    res.status(201).json({
+      err: false,
+      message: 'Here is the new attack',
+      data: data,
+    })
+  } catch (err) {
+    res.status(400).json({
+      err: true,
+      message: (err as Error).message,
+      data: null,
+    })
+  }
+}
+
+export const updateAttack = async (req: Request, res: Response) => {
+  try {
     const data = await createUpdateAttackService(req.body, false)
     res.status(201).json({
       err: false,
--- server/src/routes/CRUD.route.ts
+++ server/src/routes/CRUD.route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
-import { createAttack } from '../controllers/CRUD.controller'
+import { createAttack, updateAttack } from '../controllers/CRUD.controller'
 
 const router = Router()
 
 router.post('/create', createAttack)
+
+router.post('/update', updateAttack)
 
 export default router
