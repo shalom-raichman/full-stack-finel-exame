9def50ce7fef76c9105394fa5706d0c031dd02f1
Shalom <shlomo000@gmail.com>
2024-12-24 12:33:44 +0200

    add CreateAttackDTO

---
+++ client/src/types/createAttackDTO.ts
@@ -0,0 +1,12 @@
+export interface CreateAttackDTO {
+  nkill: number
+  nwound: number
+  city: string
+  iyear: number
+  imonth: number
+  latitude: number
+  longitude: number
+  gname: string
+  attacktype1_txt: string
+  summary: string
+}
