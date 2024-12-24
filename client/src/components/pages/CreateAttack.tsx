59b6c2ee65cc713e57bf4a965c8927f9eda20e28
Shalom <shlomo000@gmail.com>
2024-12-24 15:38:30 +0200

    add root to the create attack comp

---
+++ client/src/components/pages/CreateAttack.tsx
@@ -0,0 +1,11 @@
+import CreateAttackForm from './CreateAttackForm'
+
+const CreateAttack = () => {
+  return (
+    <div className='page create-attack'>
+      <CreateAttackForm />
+    </div>
+  )
+}
+
+export default CreateAttack
